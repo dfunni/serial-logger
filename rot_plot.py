66089cf2db22fd151f1a0b4f18738846070e7870
dfunni <dfunni@gmail.com>
2020-03-27 15:51:03 -0700

    updated

--- rot_plot.py
+++ rot_plot.py
@@ -4,22 +4,25 @@ import csv
 
 d = []
 with open('log.txt', newline='\n') as f:
-	reader = csv.reader(f, 'excel-tab')
-	for row in reader:
-		if len(row) == 4:
-			r = [float(n) for n in row]
-			d.append(r)
+    reader = csv.reader(f, 'excel-tab')
+    try:
+        for row in reader:
+            if len(row) == 4:
+                r = [float(n) for n in row]
+                d.append(r)
+    except:
+        pass
 
-d = np.asarray(d)		
+d = np.asarray(d)
 x = d[:,0]
-#plt.scatter(x, d[:,1], label='roll')
-#plt.scatter(x, d[:,2], label='pitch')
-#plt.scatter(x, d[:,3], label='yaw')
-#plt.legend()
-#plt.show()
-		
-plt.plot(x, d[:,1], label='roll')
-plt.plot(x, d[:,2], label='pitch')
-plt.plot(x, d[:,3], label='yaw')
+plt.scatter(x, d[:,1], label='roll')
+plt.scatter(x, d[:,2], label='pitch')
+plt.scatter(x, d[:,3], label='yaw')
 plt.legend()
 plt.show()
+
+# plt.plot(x, d[:,1], label='roll')
+# plt.plot(x, d[:,2], label='pitch')
+# plt.plot(x, d[:,3], label='yaw')
+# plt.legend()
+# plt.show()
